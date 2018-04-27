5e522acadd3b3cee52396546d42bc25581e2c2dc
JasonYangCIS <jasonYangCIS@gmail.com>
2018-04-26 22:23:11 -0700

    finished homepage slider

--- wp-content/themes/jlsmartbets/front-page.php
+++ wp-content/themes/jlsmartbets/front-page.php
@@ -16,11 +16,21 @@
       <?php if(have_posts()): while (have_posts()) : the_post(); ?>
         <?php if ( has_post_thumbnail() ): ?>
           <?php
-          $custom = get_post_custom(get_the_ID());  
-          $link =  $custom["link1"][0]; ?> 
+          $custom = get_post_custom(get_the_ID()); ?> 
           <?php if ( has_post_thumbnail()) : ?>      
-            <li class="slide" style="background: url( <?php $img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); echo $img[0]; ?>) no-repeat;" >
-              <a href="<?php echo $link;  ?>"> <div class="text"><?php echo get_the_content(); ?></div> </a>
+            <li class="slide">
+              <img src="<?php $img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); echo $img[0]; ?>" alt=""/>
+              <div class="slider-content-container">
+                <div class="wrapper">
+                  <div class="slider-inner-container">
+                    <div class="slider-content">
+                      <?php echo get_field('slider_content'); ?>
+                      <div class="slider-button"><?php echo get_field('slider_button'); ?></div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
             </li>
           <?php endif; ?>
         <?php endif; ?>
@@ -29,23 +39,23 @@
   </div>
   <?php wp_reset_query(); ?>
 
-<section class="content content-full front-page-content">
-  <article>
+  <section class="content content-full front-page-content">
+    <article>
 
-    <?php query_posts( array('post_type' => 'frontpage_info','posts_per_page'=>4, 'orderby' => 'menu_order', 'order' => 'ASC' ,  'post_status'=>'publish'  )); ?>
-    <div class="front_page_wrapper">
-      <?php if(have_posts()):
-      while (have_posts()) :  the_post(); ?>
-      <div class="home-block-container">
-        <?php echo'<h6>'; the_title(); echo'</h6>';  ?>
-        <div class="frontpage_info">
-          <?php echo the_content('Read more...'); ?>
+      <?php query_posts( array('post_type' => 'frontpage_info','posts_per_page'=>4, 'orderby' => 'menu_order', 'order' => 'ASC' ,  'post_status'=>'publish'  )); ?>
+      <div class="front_page_wrapper">
+        <?php if(have_posts()):
+        while (have_posts()) :  the_post(); ?>
+        <div class="home-block-container">
+          <?php echo'<h6>'; the_title(); echo'</h6>';  ?>
+          <div class="frontpage_info">
+            <?php echo the_content('Read more...'); ?>
+          </div>
         </div>
-      </div>
-    <?php endwhile; endif;?>
-  </div>
-  <?php wp_reset_query(); ?>
-</article>
+      <?php endwhile; endif;?>
+    </div>
+    <?php wp_reset_query(); ?>
+  </article>
 </section>
 
 
